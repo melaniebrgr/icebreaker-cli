bc8615118e2133f6a80d9f185ee799c2c405d50e
Melanie Burger <melanie.brgr@gmail.com>
2021-01-10 10:57:31 +0100

    💅

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,21 @@
 #!/usr/bin/env node
 
+const chalk = require("chalk");
+const boxen = require("boxen");
 const { data } = require("./data.json");
 
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
 const getRandomArrayElement = (arr) => arr[getRandomNumber(arr.length - 1)];
 
-console.log( getRandomArrayElement(data).question );
+const question = getRandomArrayElement(data).question;
+
+console.log(`
+  Question no. ${data.findIndex(el => el.question === question) + 1}`);
+
+console.log(boxen( chalk.white.bold(question), {
+    padding: 1,
+    margin: 0.25,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "#555555"
+   }));
