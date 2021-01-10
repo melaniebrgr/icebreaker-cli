1fc457d8ed9010a435ce2a5644e51fbc4ba675dd
Melanie Burger <melanie.brgr@gmail.com>
2021-01-10 10:45:39 +0100

    ♻️

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,6 @@
 const { data } = require("./data.json");
 
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
-const getRandomListItem = (arr) => arr[getRandomNumber(arr.length - 1)];
+const getRandomArrayElement = (arr) => arr[getRandomNumber(arr.length - 1)];
 
-console.log( getRandomListItem(data).question );
+console.log( getRandomArrayElement(data).question );
